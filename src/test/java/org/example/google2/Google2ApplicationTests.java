b97c0bd4cf02e44db407a8e5bfeca4132cccd7aa
BusecanAntonio <xmaxxantono@yahoo.com>
2025-10-20 11:22:51 +0300

    initializare

---
+++ src/test/java/org/example/google2/Google2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.google2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Google2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
